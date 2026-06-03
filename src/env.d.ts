e5e066fbab080ea729e45c50b60b7aaff241bd4f
visatk <visatk29@gmail.com>
2026-06-03 12:02:41 +0600

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,14 @@
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+type D1Database = import("@cloudflare/workers-types").D1Database;
+
+interface Env {
+  DB: D1Database;
+}
+
 type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 
 declare namespace App {
-	interface Locals extends Runtime {}
+  interface Locals extends Runtime {}
 }
